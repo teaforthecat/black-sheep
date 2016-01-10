0e9f53fb219e690bf84d3483ba510c6c08d254a5
Chris Thompson <chris.thompson@govdelivery.com>
2016-01-09 19:28:06 -0600

    initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject black-sheep "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 ;; TODO: confirm. this may or may not be a requirement
+                 ;; [org.clojure/clojure "1.5.1"]
+                 [overtone "0.9.1"]])
---
+++ src/black_sheep/core.clj
@@ -0,0 +1,6 @@
+(ns black-sheep.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
